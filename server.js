325414293f570268018d98df4fad4c577aa9e89c
Diane Lenertz <dlenertz@tableau.com>
2018-05-08 15:39:56 -0700

    idk

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var app = express();
-// ver path = require('path')
+ver path = require('path')
 var port = process.env.PORT || 3000;
+
 app.listen(port, "0.0.0.0", function() {
 console.log("Listening on Port 3000");
 });
 
-// app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', function(req,res) {
-
-res.sendFile(__dirname+'/dist');
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/app/index.html'));
 });
 
 // app.get('/', function (req, res) {
