a6c91f9b956220bfe096dabb8bb2605394656714
Diane Lenertz <dlenertz@tableau.com>
2018-05-08 11:47:14 -0700

    change server.js

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ app.listen(port, "0.0.0.0", function() {
 console.log("Listening on Port 3000");
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/src/app/index.html');
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/app/index.html'));
 });
+
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/src/app/index.html');
+// });
