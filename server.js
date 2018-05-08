aa67d0d815a873051ce739e46efb22f9f31a80eb
Diane Lenertz <dlenertz@tableau.com>
2018-05-08 15:31:41 -0700

    no path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+// ver path = require('path')
 var port = process.env.PORT || 3000;
 app.listen(port, "0.0.0.0", function() {
 console.log("Listening on Port 3000");
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/app/index.html'));
+res.sendFile(__dirname+'/dist/app/index.html');
 });
 
 // app.get('/', function (req, res) {
