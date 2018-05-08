6dc40feadddd356b841a2f3e4bb73b258eac9219
Diane Lenertz <dlenertz@tableau.com>
2018-05-08 14:54:56 -0700

    fix pathname

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/app/index.html'));
+res.sendFile(path.join(__dirname+'/dist/src/app/index.html'));
 });
 
 // app.get('/', function (req, res) {
